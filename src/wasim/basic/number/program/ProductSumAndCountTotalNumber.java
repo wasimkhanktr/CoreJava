fea8ded5f8e5cc3ba7cd0896f9d645063b290947
MD WASIM AKRAM <wasimkhanktr@gmail.com>
2024-09-07 19:15:12 +0530

    Add print statements to display total count, sum, and product of numbers

---
+++ src/wasim/basic/number/program/ProductSumAndCountTotalNumber.java
@@ -0,0 +1,34 @@
+package wasim.basic.number.program;
+
+import java.util.Scanner;
+
+public class ProductSumAndCountTotalNumber {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.print("Enter the starting number :");
+	
+		int n=sc.nextInt();
+		
+		System.out.print("Enter the ending number :");
+		
+		int m=sc.nextInt();
+		
+		int sum=0,count=0,product=1;
+		
+		for(int i=n; i<=m; i++)
+		{
+			sum=sum+i;
+			product=product*i;
+			count++;
+		}
+		System.out.println("--------------------------------------");
+		System.out.println("Total number is "+count);
+		System.out.println("Sum of total number is : "+sum);
+		System.out.println("Product of total number is : "+product);
+		
+
+	}
+
+}
