ddd7bb6fb475b4d556b1ebbe7326a559d8c35e62
MD WASIM AKRAM <wasimkhanktr@gmail.com>
2024-09-07 19:53:36 +0530

    Display the entered number is PrimeNumber

---
+++ src/wasim/basic/number/program/PrimeNumber.java
@@ -0,0 +1,26 @@
+package wasim.basic.number.program;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a Number : ");
+		int n=sc.nextInt();
+		int count=0;
+		
+		for(int i=1; i<=n; i++)
+		{
+			if(n%i==0)
+			count++;
+		}
+		if(count==2)
+			System.out.println(n+" is a prime number");
+		else
+			System.out.println(n+" is not a prime number");
+		
+
+	}
+
+}
