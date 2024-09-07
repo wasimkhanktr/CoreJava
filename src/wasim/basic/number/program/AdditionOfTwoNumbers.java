b7fb28a347c1652f7ae7e20ab4e725f0ad7b72c6
MD WASIM AKRAM <wasimkhanktr@gmail.com>
2024-09-07 17:57:44 +0530

    Addition of two numbers

---
+++ src/wasim/basic/number/program/AdditionOfTwoNumbers.java
@@ -0,0 +1,14 @@
+package wasim.basic.number.program;
+
+public class AdditionOfTwoNumbers {
+
+	public static void main(String[] args) {
+		int a=5;
+		int b=7;
+		System.out.println("first number is : "+a);
+		System.out.println("Second number is : "+b);
+		
+		System.out.println("Addition is "+a+b);
+	}
+
+}
