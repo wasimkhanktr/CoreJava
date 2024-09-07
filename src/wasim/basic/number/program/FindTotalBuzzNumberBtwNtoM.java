beeb395440f7a11436bfc44655e5b100071f4c29
MD WASIM AKRAM <wasimkhanktr@gmail.com>
2024-09-07 19:41:26 +0530

    Display the Total BuzzNumber from m to n

---
+++ src/wasim/basic/number/program/FindTotalBuzzNumberBtwNtoM.java
@@ -0,0 +1,30 @@
+package wasim.basic.number.program;
+
+import java.util.Scanner;
+
+public class FindTotalBuzzNumberBtwNtoM {
+
+	public static void main(String[] args) {
+Scanner sc=new Scanner(System.in);
+		
+		System.out.print("Enter the starting number :");
+	
+		int n=sc.nextInt();
+		
+		System.out.print("Enter the ending number :");
+		
+		int m=sc.nextInt();
+		
+		for(int i=n; i<=m; i++)
+		{
+			if(i%7==0)
+			{
+				int rem=i%10;
+				if(rem==7)
+					System.out.print(i+" ");
+			}
+		}
+
+	}
+
+}
