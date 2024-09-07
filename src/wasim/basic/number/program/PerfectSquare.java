f5b4e5059b0bfd1dc8ef8a476d0cc684ff1de067
MD WASIM AKRAM <wasimkhanktr@gmail.com>
2024-09-08 01:39:45 +0530

    Display the number is PerfectSquare or Not?

---
+++ src/wasim/basic/number/program/PerfectSquare.java
@@ -0,0 +1,26 @@
+package wasim.basic.number.program;
+
+import java.util.Scanner;
+
+public class PerfectSquare {
+
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number : ");
+		int n=sc.nextInt();
+		boolean flag=false;
+		for(int i=1; i<=n; i++)
+		{
+			if(i*i==n)
+				flag=true;
+		}
+		if(flag)
+			System.out.print(n+" is a Perfect Squere ");
+		else
+			System.out.print(n+" is not a Perfect Squere ");
+		
+
+	}
+
+}
