8499c5adeda07b23edacf8ca2a11d67b663f3864
MD WASIM AKRAM <wasimkhanktr@gmail.com>
2024-09-07 17:50:36 +0530

    Create program to swap two numbers without using a third variable

---
+++ src/wasim/basic/number/program/SwapTwoNoWithoutThirdVariable.java
@@ -0,0 +1,19 @@
+package wasim.basic.number.program;
+
+public class SwapTwoNoWithoutThirdVariable {
+
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 7;
+		System.out.println("Before swap number is " + a + " " + b);
+		
+//		without 2rd variable there many rule mulple and add etc
+		a=a+b;
+		b=a-b;
+		a=a-b;
+		
+		System.out.println("After swap number is " + a + " " + b);
+
+	}
+
+}
