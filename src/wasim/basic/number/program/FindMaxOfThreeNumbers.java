81cb85f3b79073ac4042c241e2d8012f06a28305
MD WASIM AKRAM <wasimkhanktr@gmail.com>
2024-09-07 18:06:25 +0530

    Compare three numbers and print the largest value

---
+++ src/wasim/basic/number/program/FindMaxOfThreeNumbers.java
@@ -0,0 +1,19 @@
+package wasim.basic.number.program;
+
+public class FindMaxOfThreeNumbers {
+
+	public static void main(String[] args) {
+		int a=5,b=6,c=7;
+		
+		System.out.println("Numbers are "+a+","+b+" and "+c);
+		
+//		find max
+		if(a>b && a>c)
+			System.out.println(a+" is Greater");
+		else if(b>c)
+			System.out.println(b+" is Greater");
+		else
+			System.out.println(c+" is Greater");
+	}
+
+}
