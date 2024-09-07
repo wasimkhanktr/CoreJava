78068536bb4898bd34bfe1572dc60411a0db6691
MD WASIM AKRAM <wasimkhanktr@gmail.com>
2024-09-07 17:44:59 +0530

    Create program to swap two numbers using a third variable

--- src/wasim/basic/number/program/SwapTwoNumber.java
+++ src/wasim/basic/number/program/SwapTwoNumber.java
@@ -2,8 +2,18 @@ package wasim.basic.number.program;
 
 public class SwapTwoNumber {
 
+	// swap two number with the help of third variable
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int a = 5;
+		int b = 7;
+		int c;
+		System.out.println("Before swap number is " + a + " " + b);
+
+		c = a;
+		a = b;
+		b = c;
+
+		System.out.println("After swap number is " + a + " " + b);
 
 	}
 
