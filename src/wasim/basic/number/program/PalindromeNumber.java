a4cd941d206c6a439eca31ced8deb0d4ce214ecd
MD WASIM AKRAM <wasimkhanktr@gmail.com>
2024-09-09 01:32:56 +0530

    Display input number is a palindrome number or Not

---
+++ src/wasim/basic/number/program/PalindromeNumber.java
@@ -0,0 +1,29 @@
+//A palindromic number is a number that remains the same when 
+//its digits are reversed. Like 16461, for example,
+
+
+package wasim.basic.number.program;
+
+import java.util.Scanner;
+
+public class PalindromeNumber {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a Number : ");
+		int n=sc.nextInt();
+		int temp=n,palindrome=0;
+		while(temp>0)
+		{
+			int rem=temp%10;
+			palindrome=palindrome*10+rem;
+			temp=temp/10;
+		}
+		if(n==palindrome)
+			System.out.println(n+" is a palindrome Number");
+		else
+			System.out.println(n+" is not a palindrome Number");
+
+	}
+
+}
