eee331823a5f989879688e15cc592b3acf8a152f
MD WASIM AKRAM <wasimkhanktr@gmail.com>
2024-09-08 12:00:29 +0530

    Display the number is AutoMorphic or Not?

---
+++ src/wasim/basic/number/program/AutomorphicNumber.java
@@ -0,0 +1,30 @@
+package wasim.basic.number.program;
+
+import java.util.Scanner;
+
+public class AutomorphicNumber {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int n = sc.nextInt();
+        
+        // Get the square of the number
+        int square = n * n;
+        
+        // Convert both number and its square to strings
+        String numStr = String.valueOf(n);
+        String squareStr = String.valueOf(square);
+        
+        // Check if the square ends with the original number
+        if (squareStr.endsWith(numStr)) {
+            System.out.println(n + " is an Automorphic Number. Square of " + n + " is " + square);
+        } else {
+            System.out.println(n + " is not an Automorphic Number.");
+        }
+        
+        // Close the scanner
+        sc.close();
+    }
+}
