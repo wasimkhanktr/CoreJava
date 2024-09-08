cbd6be4bff2f5c8fffead5092f0614478fdd01a4
MD WASIM AKRAM <wasimkhanktr@gmail.com>
2024-09-09 01:43:29 +0530

    Display input number is a Reverse Number or Not

---
+++ src/wasim/basic/number/program/ReverceNumber.java
@@ -0,0 +1,22 @@
+package wasim.basic.number.program;
+
+import java.util.Scanner;
+
+public class ReverceNumber {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a Number : ");
+		int n=sc.nextInt();
+		int temp=n,reverse=0;
+		while(temp>0)
+		{
+			int rem=temp%10;
+			reverse=reverse*10+rem;
+			temp=temp/10;
+		}
+		System.out.println(reverse+" is a reverse Number of "+n);
+
+	}
+
+}
