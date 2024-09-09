f356d8a328809aa1454573d3c7dd5baa8132b4b1
MD WASIM AKRAM <wasimkhanktr@gmail.com>
2024-09-09 15:34:17 +0530

    Display input number is a Strong Number or Not

---
+++ src/wasim/basic/number/program/StrongNumber.java
@@ -0,0 +1,34 @@
+//A Number that can be represented as the sum of the factorial of it's individual digits is known as a Strong Number.
+//ex- 145-> 1!+4!+5!= 1 + 1*2*3*4 + 1*2*3*4*5 = 1+ 24 + 120 = 145 
+
+package wasim.basic.number.program;
+
+import java.util.Scanner;
+
+public class StrongNumber {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a number : ");
+		int n=sc.nextInt();
+		int temp=n,sum=0;
+		
+		while(temp>0)
+		{
+			int rem=temp%10;
+			int product=1;
+			for(int i=1; i<=rem; i++)
+			{
+				product=product*i;
+			}
+			sum=sum+product;
+			temp=temp/10;
+		}
+		if(n==sum)
+			System.out.println(n+" is a Strong Number ");
+		else
+			System.out.println(n+" is not a Strong Number ");
+
+	}
+
+}
